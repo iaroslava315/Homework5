9f4c314bdc35c8103909aa5f3d33d1b4a204d656
iaroslava315 <iaroslava.315@gmail.com>
2026-04-14 23:03:35 +0700

    исправление 2

--- src/Main.java
+++ src/Main.java
@@ -17,18 +17,16 @@ void main() {
 
 //  Task 2
     {
-        int clientOS = 0;
-        int clientDeviceYear = 2015;
-        if (clientOS == 0) {
+        int clientOS = 1;
+        int clientDeviceYear = 2014;
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         } else if (clientOS == 0 && clientDeviceYear < 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOS == 1) {
+        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (clientOS == 1 && clientDeviceYear < 2015) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else {
-            System.out.println("Такого варианта не сущестует");
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
     }
 
@@ -36,7 +34,7 @@ void main() {
 //   Task 3
     {
         int year = 2021;
-        if ((year > 1584) && (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        if (year > 1584 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)) {
             System.out.println(year + " год является високосным");
         } else if (year > 1584) {
             System.out.println(year + " год не является високосным");
@@ -50,13 +48,13 @@ void main() {
     {
         int deliveryDistance = 95;
         int deliveryDays = 1;
-        if (deliveryDistance < 20) {
-        System.out.println("Потребуется дней: " + deliveryDays);
+        if (deliveryDistance >= 0 && deliveryDistance < 20) {
+            System.out.println("Потребуется дней: " + deliveryDays);
         } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
             deliveryDays += 1;
             System.out.println("Потребуется дней: " + deliveryDays);
         } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
-            deliveryDays +=2;
+            deliveryDays += 2;
             System.out.println("Потребуется дней: " + deliveryDays);
         } else {
             System.out.println("Доставки нет");
